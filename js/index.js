5942fca90ca13010a9711013394b9f3136cdc4c3
Polina <kuranovapolina@gmail.com>
2024-03-31 15:47:13 +0300

    output static stats

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 async function startPage()
 {
-  console.log("data");
+    console.log("startPage");
     await fetch('/stats', {method: 'POST'}).then(response => response.json()).then(async data => {
         console.log(data);
+
+        document.getElementById("CPUTemp").value = data["CPUTemp"];
+        document.getElementById("GPUTemp").value = data["GPUTemp"];
+        document.getElementById("CPULoad").value = data["CPULoad"];
+        document.getElementById("GPULoad").value = data["GPULoad"];
+        document.getElementById("RAMLoad").value = data["RAMLoad"];
     });
 }
