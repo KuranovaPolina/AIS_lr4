413969d6d8d98c7e9a2ff727832b4ba66ad1305e
Polina <kuranovapolina@gmail.com>
2024-04-01 21:33:23 +0300

    output grafics with setting frequency

--- app.py
+++ app.py
@@ -103,6 +103,73 @@ def get_stat():
                    RAMLoads = RAMLoads
                    )
 
+@app.route('/updateGraph', methods=['POST'])
+def updateGraph():
+    print("stats request")
+
+    datetimes = []
+    CPUTemps = []
+    GPUTemps = []
+    CPULoads = []
+    GPULoads = []
+    RAMLoads = []
+
+    start_date = datetime.strptime('2024-04-01 13:50', '%Y-%m-%d %H:%M')
+    end_date = datetime.strptime('2024-04-01 15:50', '%Y-%m-%d %H:%M')
+
+    print(request.json['freq'])
+
+    inter = interval.interval_generator(start_date, end_date, request.json['freq'])
+
+    try:
+        conn = psycopg2.connect("dbname='logger' user='polina' host='kuranov.sknt.ru' port='8000' password='****'")
+        # print(conn)
+    except:
+        print("I am unable to connect to the database")
+        exit(0)
+
+    with conn.cursor() as curs:
+        try:
+            # simple multi row system query
+            for value in inter:
+                # print(value)
+                query = f"select * from params where params.date_ = '{value[0]}' and params.time_ = '{value[1]}';"
+                # print(query)
+                curs.execute(query)
+
+                records = (curs.fetchmany(1))
+
+                # print(records)
+
+                datetimes.append(value[0] + " " + value[1])
+
+                if records == []:
+                    CPUTemps.append(0)
+                    GPUTemps.append(0)
+                    CPULoads.append(0)
+                    GPULoads.append(0)
+                    RAMLoads.append(0)
+                else:
+                    CPUTemps.append(records[0][2])
+                    GPUTemps.append(records[0][3])
+                    CPULoads.append(records[0][4])
+                    GPULoads.append(records[0][5])
+                    RAMLoads.append(records[0][6])
+
+        # a more robust way of handling errors
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    # print(datetimes)
+
+    return jsonify( datetimes = datetimes,
+                   CPUTemps = CPUTemps,
+                   GPUTemps = GPUTemps,  
+                   CPULoads = CPULoads,
+                   GPULoads = GPULoads,
+                   RAMLoads = RAMLoads
+                   )
+
 @app.route('/updateCritical', methods=['POST'])
 def updateCritical():
     print("updateCritical request")
--- js/index.js
+++ js/index.js
@@ -16,7 +16,63 @@ async function startPage()
         document.getElementById("GPULoad_critical").value = data["GPULoad_critical"];
         document.getElementById("RAMLoad_critical").value = data["RAMLoad_critical"];
 
-        
+        var dataChart = {
+            labels: data["datetimes"],
+            datasets: [
+                {
+                    title: "CPUTemp",
+                    data: data["CPUTemps"],
+                    backgroundColor: ['#a00'],
+                    borderWidth: 0.5,
+                    borderColor: '#ddd'
+                },
+                {
+                    title: "GPUTemp",
+                    data: data["GPUTemps"],
+                    backgroundColor: ['#a0a'],
+                    borderWidth: 0.5,
+                    borderColor: '#ddd'
+                },
+                {
+                    title: "CPULoad",
+                    data: data["CPULoads"],
+                    backgroundColor: ['#aa0'],
+                    borderWidth: 0.5,
+                    borderColor: '#ddd'
+                },
+                {
+                    title: "GPULoad",
+                    data: data["GPULoads"],
+                    backgroundColor: ['#a55'],
+                    borderWidth: 0.5,
+                    borderColor: '#ddd'
+                },
+                {
+                    title: "RAMLoad",
+                    data: data["RAMLoads"],
+                    backgroundColor: ['#fa0'],
+                    borderWidth: 0.5,
+                    borderColor: '#ddd'
+                }]
+        };
+
+        ctx = document.getElementById('graph');
+
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: dataChart,
+            options: {
+                legend: {
+                    display: true,
+                    position: 'bottom',
+                    labels: {
+                        boxWidth: 15,
+                        // fontColor: '#111',
+                        padding: 7
+                    }
+                }
+            }
+        });
     });
 }
 
@@ -42,4 +98,98 @@ async function updateCritical()
             GPULoad: (+GPULoad), 
             RAMLoad: (+RAMLoad)
         })});
+}
+
+async function updateGraph()
+{
+    console.log("updateGraph");
+
+    freq = 0;
+
+    if (document.getElementById('minute').checked) {
+        freq = 0;
+    }
+    else if (document.getElementById('hour').checked) {
+        freq = 1;
+    }
+    else if (document.getElementById('day').checked) {
+        freq = 2;
+    }
+    else if (document.getElementById('week').checked) {
+        freq = 3;
+    }
+
+    await fetch('/updateGraph', {method: 'POST',  
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify({
+            freq: freq
+        })}).then(response => response.json()).then(async data => {
+            console.log(data);
+
+            var dataChart = {
+                labels: data["datetimes"],
+                datasets: [
+                    {
+                        title: "CPUTemp",
+                        data: data["CPUTemps"],
+                        backgroundColor: ['#a00'],
+                        borderWidth: 0.5,
+                        borderColor: '#ddd'
+                    },
+                    {
+                        title: "GPUTemp",
+                        data: data["GPUTemps"],
+                        backgroundColor: ['#a0a'],
+                        borderWidth: 0.5,
+                        borderColor: '#ddd'
+                    },
+                    {
+                        title: "CPULoad",
+                        data: data["CPULoads"],
+                        backgroundColor: ['#aa0'],
+                        borderWidth: 0.5,
+                        borderColor: '#ddd'
+                    },
+                    {
+                        title: "GPULoad",
+                        data: data["GPULoads"],
+                        backgroundColor: ['#a55'],
+                        borderWidth: 0.5,
+                        borderColor: '#ddd'
+                    },
+                    {
+                        title: "RAMLoad",
+                        data: data["RAMLoads"],
+                        backgroundColor: ['#fa0'],
+                        borderWidth: 0.5,
+                        borderColor: '#ddd'
+                    }]
+            };
+
+            document.getElementById('canvas').innerHTML = "";
+
+            var ctx = document.createElement('canvas');
+            ctx.setAttribute("id", "graph");
+
+            document.getElementById('canvas').appendChild(ctx);
+
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: dataChart,
+                options: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        labels: {
+                            boxWidth: 15,
+                            // fontColor: '#111',
+                            padding: 7
+                        }
+                    }
+                }
+            });
+    });
 }
